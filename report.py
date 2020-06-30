ad5dc16d309a434f1b8fddac11feb9b3ab9a8b76
knightfury1493 <suhaibahmmad1493@gmail.com>
2020-06-30 21:25:04 +0600

    Added total BD Stat.

--- report.py
+++ report.py
@@ -12,19 +12,33 @@ class Report():
 
         logger.info("This is from another module")
 
-    def Email(count, prev_count, update_time):
-
+    def Email(reported_data):
         logger = Logger.create_logger(__name__, "INFO")
+
+        # BD
+        last_day_pos = reported_data['positive'][0]['last24']
+        tot_pos = reported_data['positive'][0]['total']
+        last_day_death = reported_data['death'][0]['last24']
+        tot_death = reported_data['death'][0]['total']
+        updated_time_BD = reported_data['updated_time_BD']
+
+        # District/Tangail
+        tan_td = reported_data['district'][0]['count']
+        tan_prev = reported_data['district'][0]['prev_count']
+        updated_time_dist = reported_data['district'][0]['updated_on']
 
         EMAILL_ADDRESS = os.environ.get('USER_EMAIL')
         EMAIL_PASSWARD = os.environ.get('USER_PASSWARD')
 
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
             smtp.login(EMAILL_ADDRESS, EMAIL_PASSWARD)
-            subject = 'Tangail Report from Python bot'
-            body = f"Present count {count}, previous count {prev_count}."
-            body += "\nIncrease of {} person.".format(int(count) - int(prev_count))
-            body += "\n\nAs per {}".format(update_time)
+            subject = 'Report from Python bot'
+            body = f"Positive, last day {int(last_day_pos):,}, total positive {int(tot_pos):,}."
+            body += f"\nDeath, last day {last_day_death}, total death {int(tot_death):,}."
+            body += f"\nAs per {updated_time_BD}"
+            body += f"\n\nIn Tangail today's positive {tan_td}, previous day {tan_prev}."
+            body += f"\nIncrease of {int(tan_td)-int(tan_prev)}."
+            body += f"\nAs per {updated_time_dist}"
             msg = f"Subject: {subject}\n\n{body}"
             smtp.sendmail(EMAILL_ADDRESS, 'suhaib16@student.sust.edu', msg)
             logger.info("Email Sent")
--- save_data_auto.py
+++ save_data_auto.py
@@ -11,7 +11,22 @@ from logger import Logger
 # Creating a logger handler
 logger = Logger.create_logger(__name__, 'INFO')
 
-def job():
+to_report = {}
+
+def time_converter(str_dt):
+
+    dt = datetime.datetime.strptime(str_dt, "%a, %d %b %Y %H:%M:%S %Z")  # String to date
+
+    utc_tz = pytz.timezone("UTC")  # UTC timezone
+
+    dt = utc_tz.localize(dt)  # Localizing dt to UTC time
+
+    dt_dhk = dt.astimezone(pytz.timezone("Asia/Dhaka"))  # Converting dt to dhaka time
+
+    return (dt_dhk.strftime("%a, %d %b %Y %I:%M:%S %p %Z"))  # Converting to interested string
+
+
+def get_dist_data():
 
     logger.info("......STARTING DISTRICT DATA SYNC.....")
 
@@ -29,29 +44,71 @@ def job():
         data = json.loads(p)
 
         # Converting updated time string to datetime UTC+6
-        str_dt = data['updated_on']
-
-        dt = datetime.datetime.strptime(str_dt, "%a, %d %b %Y %H:%M:%S %Z")  # String to date
-
-        utc_tz = pytz.timezone("UTC")  # UTC timezone
-
-        dt = utc_tz.localize(dt)  # Localizing dt to UTC time
-
-        dt_dhk = dt.astimezone(pytz.timezone("Asia/Dhaka"))  # Converting dt to dhaka time
-
-        updated_time = dt_dhk.strftime("%a, %d %b %Y %I:%M:%S %p %Z")  # Converting to interested string
+        updated_time_dist = time_converter(data['updated_on'])
 
         # Searching through data to find Tangail
         for data in data["data"]:
             if data['name'] == "Tangail":
                 # If present count is greater than prev_count send email
                 if data['count'] > data['prev_count']:
-                    Report.Email(data['count'], data['prev_count'], updated_time)
+                    # Report.Email(data['count'], data['prev_count'], updated_time_dist)
+                    to_report['district'] = []
+                    to_report['district'].append({
+                        "name": "Tangail",
+                        "count": data['count'],
+                        "prev_count": data["prev_count"],
+                        "updated_on": updated_time_dist
+                    })
                 else:
                     logger.info("No change in data of Tangail.")
 
     except Exception as e:
-        logger.info("Could not connect...Check internet")
+        logger.info("Could not connect to district data...Check internet")
+
+    logger.info("......FINISH.....")
+
+
+def get_total_data():
+    logger.info("......STARTING BD DATA SYNC.....")
+
+    try:
+        # Requesting the site
+        source = requests.get("https://corona-bd.herokuapp.com/stats")
+
+        # Creating a beautifulsoup object
+        soup = BeautifulSoup(source.content, "lxml")
+
+        # Finding the paragraph in soup object
+        p = soup.find('p').text
+
+        # Loads it in json
+        data = json.loads(p)
+
+        to_report["death"] = []
+
+        to_report['death'].append(data["death"])
+
+        to_report["positive"] = []
+
+        to_report["positive"].append(data["positive"])
+
+        # Converting updated time string to datetime UTC+6
+        updated_time_BD = time_converter(data['updated_on'])
+
+        to_report['updated_time_BD'] = updated_time_BD
+
+    except Exception as e:
+        logger.info("Could not connect to BD data...Check internet")
+
+    logger.info("......FINISH.....")
+
+def job():
+
+    get_dist_data()
+
+    get_total_data()
+
+    Report.Email(to_report)
 
 
 # def job():
@@ -68,9 +125,9 @@ def job():
 
 
 # # schedule.every(2).hours.do(job)
-# schedule.every(1).minutes.do(job)
+schedule.every(2).minutes.do(job)
 # # schedule.every().hour.do(job)
-schedule.every().day.at("12:00").do(job)
+# schedule.every().day.at("12:00").do(job)
 # # schedule.every(5).to(10).minutes.do(job)
 # # schedule.every().monday.do(job)
 # # schedule.every().wednesday.at("13:15").do(job)
@@ -81,5 +138,6 @@ print("STARTING...")
 while True:
     schedule.run_pending()
     time.sleep(1)
+
 
 # job()
