f7995a198fce528e25fe84aa9b85f2d083a27646
knightfury1493 <suhaibahmmad1493@gmail.com>
2020-06-30 21:41:53 +0600

    Changed the time to 6PM everyday.

--- save_data_auto.py
+++ save_data_auto.py
@@ -125,9 +125,9 @@ def job():
 
 
 # # schedule.every(2).hours.do(job)
-schedule.every(2).minutes.do(job)
+# schedule.every(2).minutes.do(job)
 # # schedule.every().hour.do(job)
-# schedule.every().day.at("12:00").do(job)
+schedule.every().day.at("12:00").do(job)
 # # schedule.every(5).to(10).minutes.do(job)
 # # schedule.every().monday.do(job)
 # # schedule.every().wednesday.at("13:15").do(job)
